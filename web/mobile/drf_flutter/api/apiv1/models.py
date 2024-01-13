9514b87d0e7dbcc340f9b459e63fcbe790b58534
nakatsukarikuto <122096541+nakatsukarikuto@users.noreply.github.com>
2024-01-13 17:00:20 +0900

    DjangoをRestful API化

---
+++ web/mobile/drf_flutter/api/apiv1/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class TestData(models.Model):
+
+    testname = models.CharField(max_length=100)
+    testvalue = models.IntegerField()
---
+++ web/mobile/drf_flutter/api/apiv1/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import TestData
+from .serializers import TestDataSerializer
+
+# Create your views here.
+
+class TestDataViewSet(viewsets.ModelViewSet):
+    queryset = TestData.objects.all()
+    serializer_class = TestDataSerializer()
